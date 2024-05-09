5ae434df079b0818bcff4caf06ad1f1dc48d2316
abdelazizeeeee <abdelaziz.naija@horizon-tech.tn>
2024-05-09 16:23:10 +0100

    first commit

--- setup.py
+++ setup.py
@@ -5,26 +5,22 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+    long_description = fh.read()
 
 VERSION = "0.1.0"
-DESCRIPTION = (
-    "Get IP addresses from a website using Selenium and Chrome WebDriver"
-)
-LONG_DESCRIPTION = (
-    "Get IP addresses from a website using Selenium and Chrome WebDriver"
-)
+DESCRIPTION = "Get IP addresses from a website using Selenium and Chrome WebDriver"
+
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
-# Setting up
+
 setup(
     name="IPAdresseScraping",
     version=VERSION,
-    author0="Abdelaziz NAIJA",
-    author_email="<abdelaziz.naija@horizon-tech.tn>",
+    author="Abdelaziz NAIJA",
+    author_email="abdelaziz.naija@horizon-tech.tn",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=requirements,
     keywords=["python", "IP", "Selenium", "WebDriver", "Scraping"],
